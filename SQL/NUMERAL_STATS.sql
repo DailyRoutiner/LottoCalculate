eb5e5378bfd6541b999662e3d9b741595f33f413
개미핥기 <@MY-PC>
2015-03-21 17:07:39 +0900

    sql 바뀐거
    numeral_stats에 기본 데이터 넣음

--- SQL/NUMERAL_STATS.sql
+++ SQL/NUMERAL_STATS.sql
@@ -19,6 +19,51 @@ FOREIGN KEY ("UNIT_ID_FK")
 REFERENCES "UNIT_STATS" ("UNIT_ID");
 
 
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (1, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (2, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (3, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (4, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (5, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (6, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (7, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (8, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (9, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (10, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (11, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (12, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (13, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (14, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (15, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (16, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (17, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (18, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (19, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (20, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (21, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (22, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (23, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (24, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (25, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (26, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (27, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (28, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (29, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (30, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (31, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (32, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (33, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (34, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (35, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (36, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (37, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (38, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (39, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (40, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (41, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (42, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (43, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (44, null);
+insert into numeral_stats (NUMBER_ID, NUMERAL_FREQUENCY) values (45, null);
 
 
 commit;
