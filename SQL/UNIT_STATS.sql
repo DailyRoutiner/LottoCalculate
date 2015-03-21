dc3eefacbb47a3956d1576289226e8678266c19a
개미핥기 <@MY-PC>
2015-03-21 15:34:30 +0900

    a

---
+++ SQL/UNIT_STATS.sql
@@ -0,0 +1,11 @@
+DROP table "UNIT_STATS";
+
+CREATE table "UNIT_STATS" (
+    "UNIT_ID"        VARCHAR2(30) NOT NULL,
+    "UNIT_NUMBER"    NUMBER,
+    "UNIT_FREQUENCY" NUMBER,
+    constraint  "UNIT_STATS_PK" primary key ("UNIT_ID")
+     
+);
+
+commit;
